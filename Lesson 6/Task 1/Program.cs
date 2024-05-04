57f6f6a63b4ff4dd8cf3d9ec4dc375434afa56d7
AlloShalom <49804826+AlloShalom@users.noreply.github.com>
2024-05-04 06:26:45 +0400

    Add files via upload

---
+++ Lesson 6/Task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+void CharOfString2(char[,] arr)
+{
+    string st = ""; // 
+    foreach (var i in arr)
+    {
+        Console.WriteLine(i);
+        st += i; // st = st + i
+    }
+    Console.WriteLine(st);
+}
+
+char[,] chars = new char[,]
+{
+    { 'a', '1' },
+    { 'c', 'd' }
+};
+CharOfString2(chars);
