5aaa192293545c86bf7d00bbc71325b30ca42723
AlloShalom <49804826+AlloShalom@users.noreply.github.com>
2024-05-04 06:52:24 +0400

    Task 1 & 2 & 3
    Added Task 1, Task 2 and Task 3

---
+++ Lesson 2/Task 1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число:");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if ((Number % 7) == 0 && (Number % 23) == 0) {
+    Console.WriteLine("Число " + Number + " кратно 7 и 23.");
+} else {Console.WriteLine("Число " + Number + " не кратно 7 и 23.");}
---
+++ Lesson 2/Task 3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int Number = new Random().Next(10, 99);
+Console.WriteLine(Number);
+
+int FirstNumber = Number / 10;
+int SecondNumber = Number % 10;
+
+if (FirstNumber > SecondNumber){
+    Console.WriteLine(FirstNumber);
+}
+else if (FirstNumber < SecondNumber){
+    Console.WriteLine(SecondNumber);
+} 
+else {Console.WriteLine("Числа равны");}
