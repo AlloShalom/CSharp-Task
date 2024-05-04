4320cd9ef0f9d6b4e2e3e9d19eb641b3d1d23a47
AlloShalom <49804826+AlloShalom@users.noreply.github.com>
2024-05-04 06:22:20 +0400

    Add files via upload

---
+++ Lesson 7/Task 2/Program.cs
@@ -0,0 +1,26 @@
+﻿﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int AkkermanFunction (int m, int n)
+{
+    if(m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0) 
+    {
+        return AkkermanFunction(m - 1, 1);
+    }
+    else 
+    {
+        return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+    }
+}
+
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"A({m}, {n}) = {AkkermanFunction(m, n)}");
---
+++ Lesson 7/Task 3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+void Reverse (int[] array, int i)
+{
+    if(i < 0) return;
+    Console.Write(array[i] + " ");
+    Reverse(array, i - 1);
+}
+
+int[] array = {17, 6, 54, 81, 4};
+Reverse(array, array.Length - 1);
